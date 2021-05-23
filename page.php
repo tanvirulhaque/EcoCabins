c5d9b45de0f9e86d51a06ff1c2515dd5c94ae154
Tanvirul Haque <tanvirulhaque123@gmail.com>
2021-05-23 19:39:06 +0600

    Add: Commetns design

--- page.php
+++ page.php
@@ -17,22 +17,33 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<div class="page-content">
+			<div class="container">
+				<div class="row">
 
-			get_template_part( 'template-parts/content', 'page' );
+					<div class="col-md-8 <?php if ( ! is_active_sidebar( 'sidebar-1' ) ) { echo 'col-md-offset-2'; } ?>">
+						<?php
+						while ( have_posts() ) :
+							the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+							get_template_part( 'template-parts/content', 'page' );
 
-		endwhile; // End of the loop.
-		?>
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+						endwhile; // End of the loop.
+						?>
+					</div>
+
+					<?php get_sidebar(); ?>
+
+				</div>
+			</div><!-- .container -->
+		</div>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
